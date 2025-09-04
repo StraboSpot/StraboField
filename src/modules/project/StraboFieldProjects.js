65433f0e2c09a938e2ed0d23685a578ca79da1aa
jessicagood <jessicagood012@gmail.com>
2025-09-04 14:46:13 -0700

    refactor(Home Menu): UI updates Backup and StraboField Projects pages to use lists instead of buttons

--- src/modules/project/StraboFieldProjects.js
+++ src/modules/project/StraboFieldProjects.js
@@ -2,22 +2,26 @@ import React from 'react';
 import {FlatList, Platform, Text, View} from 'react-native';
 
 import {Button} from '@rn-vui/base';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
-import LoadProjectButtons from './load/LoadProjectButtons';
 import useDevice from '../../services/useDevice';
 import commonStyles from '../../shared/common.styles';
+import {isEmpty} from '../../shared/Helpers';
 import {BLUE} from '../../shared/styles.constants';
+import FlatListItemSeparator from '../../shared/ui/FlatListItemSeparator';
 import overlayStyles from '../../shared/ui/modals/overlay.styles';
 import SectionDivider from '../../shared/ui/SectionDivider';
 import Spacer from '../../shared/ui/Spacer';
 import uiStyles from '../../shared/ui/ui.styles';
 import {SIDE_PANEL_VIEWS} from '../main-menu-panel/mainMenu.constants';
 import {setSidePanelVisible} from '../main-menu-panel/mainMenuPanel.slice';
+import MainMenuPanelListItem from '../main-menu-panel/MainMenuPanelListItem';
 import LogOut from '../user/LogOut';
 
 const StraboFieldProjects = () => {
   const dispatch = useDispatch();
+  const isOnline = useSelector(state => state.connections.isOnline);
+  const user = useSelector(state => state.user);
   const {openURL} = useDevice();
 
   const importLocationText = Platform.OS === 'ios' ? 'Documents/Strabofield/Distribution'
@@ -51,60 +55,58 @@ const StraboFieldProjects = () => {
         ListHeaderComponent={
           <>
             <SectionDivider dividerText={'Load a Project'}/>
-            <LoadProjectButtons
-              onLoadProjectsFromDevice={onLoadProjectsFromDevice}
-              onLoadProjectsFromDownloadsFolder={onLoadProjectsFromDownloadsFolder}
-              onLoadProjectsFromServer={onLoadProjectsFromServer}
-              onStartNewProject={onStartNewProject}
-            />
+            <MainMenuPanelListItem onPress={onStartNewProject} title={'New'}/>
+            <FlatListItemSeparator/>
+            {!isEmpty(user.name) && isOnline.isConnected && (
+              <>
+                <MainMenuPanelListItem onPress={onLoadProjectsFromServer} title={'Download'}/>
+                <FlatListItemSeparator/>
+              </>
+            )}
+            <MainMenuPanelListItem onPress={onLoadProjectsFromDevice} title={'Open'}/>
+            {!isEmpty(user.name) && !isOnline.isConnected && (
+              <Text style={{...overlayStyles.statusMessageText, fontWeight: 'bold'}}>
+                Please connect to the Internet.
+              </Text>
+            )}
+            <FlatListItemSeparator/>
+            <MainMenuPanelListItem onPress={onLoadProjectsFromDownloadsFolder} title={'Import'}/>
             <Spacer/>
             <SectionDivider dividerText={'Other Actions with\nLocal Projects on Device'}/>
-            <View style={{paddingHorizontal: 10}}>
-              <Button
-                buttonStyle={commonStyles.standardButton}
-                containerStyle={commonStyles.standardButtonContainer}
-                onPress={onDeleteLocalCopy}
-                title={'Delete'}
-                titleStyle={commonStyles.standardButtonText}
-                type={'outline'}
-              />
-              <Button
-                buttonStyle={commonStyles.standardButton}
-                containerStyle={commonStyles.standardButtonContainer}
-                onPress={onExportOtherSavedProject}
-                title={Platform.OS === 'ios' ? 'Zip' : 'Export to Zip'}
-                titleStyle={commonStyles.standardButtonText}
-                type={'outline'}
-              />
-              {Platform.OS === 'ios' && (
-                <View style={{flex: 1, justifyContent: 'flex-end', paddingBottom: 15}}>
-                  <View style={{padding: 10, alignItems: 'center'}}>
-                    <Text style={{...uiStyles.sectionDividerText, textAlign: 'center'}}>
-                      Additional help documents can be found in the Menu -&gt; Help -&gt; Documentation
-                    </Text>
-                  </View>
-                  <Button
-                    buttonStyle={commonStyles.standardButton}
-                    containerStyle={commonStyles.buttonPadding}
-                    icon={{
-                      name: 'file-tray-full-outline',
-                      type: 'ionicon',
-                      color: BLUE,
-                    }}
-                    iconContainerStyle={{paddingRight: 10}}
-                    onPress={() => openURL('ProjectBackups')}
-                    title={'View/Edit Files on Device'}
-                    titleStyle={commonStyles.standardButtonText}
-                    type={'outline'}
-                  />
+            <MainMenuPanelListItem onPress={onDeleteLocalCopy} title={'Delete'}/>
+            <FlatListItemSeparator/>
+            <MainMenuPanelListItem
+              onPress={onExportOtherSavedProject}
+              title={Platform.OS === 'ios' ? 'Zip' : 'Export to Zip'}
+            />
+            {Platform.OS === 'ios' && (
+              <View style={{flex: 1, justifyContent: 'flex-end', paddingBottom: 15}}>
+                <View style={{padding: 10, alignItems: 'center'}}>
+                  <Text style={{...uiStyles.sectionDividerText, textAlign: 'center'}}>
+                    Additional help documents can be found in the Menu -&gt; Help -&gt; Documentation
+                  </Text>
                 </View>
-              )}
-              {Platform.OS === 'android' && (
-                <Text style={overlayStyles.statusMessageText}>
-                  *The imported project must be a .zip file in the {importLocationText} folder.
-                </Text>
-              )}
-            </View>
+                <Button
+                  buttonStyle={commonStyles.standardButton}
+                  containerStyle={commonStyles.buttonPadding}
+                  icon={{
+                    name: 'file-tray-full-outline',
+                    type: 'ionicon',
+                    color: BLUE,
+                  }}
+                  iconContainerStyle={{paddingRight: 10}}
+                  onPress={() => openURL('ProjectBackups')}
+                  title={'View/Edit Files on Device'}
+                  titleStyle={commonStyles.standardButtonText}
+                  type={'outline'}
+                />
+              </View>
+            )}
+            {Platform.OS === 'android' && (
+              <Text style={overlayStyles.statusMessageText}>
+                *The imported project must be a .zip file in the {importLocationText} folder.
+              </Text>
+            )}
           </>
         }
       />
