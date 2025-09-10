23983938821a14f5c7825f53a657f11d7625c68e
jessicagood <jessicagood012@gmail.com>
2025-09-10 15:14:47 -0700

    refactor(UI): Small changes to navigator UI

--- src/modules/spots/SpotNavigator.js
+++ src/modules/spots/SpotNavigator.js
@@ -1,4 +1,5 @@
 import React, {useRef, useState} from 'react';
+import {View} from 'react-native';
 
 import {Picker} from '@react-native-picker/picker';
 import {useSelector} from 'react-redux';
@@ -6,6 +7,7 @@ import {useSelector} from 'react-redux';
 import {SpotsList, SpotsListItem} from './index';
 import {isEmpty} from '../../shared/Helpers';
 import * as themes from '../../shared/styles.constants';
+import {PRIMARY_BACKGROUND_COLOR, SECONDARY_BACKGROUND_COLOR} from '../../shared/styles.constants';
 import ListEmptyText from '../../shared/ui/ListEmptyText';
 import SectionDivider from '../../shared/ui/SectionDivider';
 import styles from '../../shared/ui/ui.styles';
@@ -36,7 +38,7 @@ const SpotNavigator = ({closeSpotsNavigator, openNotebookPanel, openSpotInNotebo
   };
 
   return (
-    <>
+    <View style={{backgroundColor: SECONDARY_BACKGROUND_COLOR, flex: 1}}>
       <SectionDivider dividerText={'Current Spot'}/>
       {isEmpty(selectedSpot) ? <ListEmptyText text={'No Selected Spot'}/>
         : <SpotsListItem onPress={openSpotInNotebook} spot={selectedSpot}/>}
@@ -45,7 +47,7 @@ const SpotNavigator = ({closeSpotsNavigator, openNotebookPanel, openSpotInNotebo
         onValueChange={setPickerKey}
         ref={pickerRef}
         selectedValue={pickerKey}
-        style={styles.sectionDividerText}
+        style={[styles.sectionDividerText, {backgroundColor: PRIMARY_BACKGROUND_COLOR}]}
       >
         <Picker.Item label={pickerLabels[pickerKeys.SPOTS]} value={pickerKeys.SPOTS}/>
         <Picker.Item label={pickerLabels[pickerKeys.IMAGES]} value={pickerKeys.IMAGES}/>
@@ -61,7 +63,7 @@ const SpotNavigator = ({closeSpotsNavigator, openNotebookPanel, openSpotInNotebo
         />
       )}
       {pickerKey === pickerKeys.REPORTS && <ReportsList openSpotInNotebook={openSpotInNotebook}/>}
-    </>
+    </View>
   );
 };
 
