e6937bd64d3de83d7b47f5b8425aa10ff2f93bc2
jessicagood <jessicagood012@gmail.com>
2025-10-02 16:58:23 -0700

    refactor(UI): Changed Dataset Preferences text

--- src/modules/project/datasets/DatasetPreferences.js
+++ src/modules/project/datasets/DatasetPreferences.js
@@ -18,17 +18,21 @@ const DatasetPreferences = () => {
     <View style={{flex: 1}}>
       <View style={{paddingHorizontal: 10, paddingBottom: 10, alignItems: 'center'}}>
         <Text style={{color: PRIMARY_TEXT_COLOR, fontSize: MEDIUM_TEXT_SIZE}}>
-          All settings may be modified later on the Datasets page. The Target dataset will be used for new Spots.
+          All settings may be modified later on the Datasets page. Images may also be downloaded later
+          from the Datasets page or individually from the Images page or in the Notebook. The Target
+          dataset will be used for new Spots.
         </Text>
       </View>
-      <SectionDivider
-        dividerText={`Datasets for project:  ${truncateText(project.description.project_name, 20)}`}/>
-      <FlatList
-        ItemSeparatorComponent={FlatListItemSeparator}
-        data={Object.values(datasets)}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => <DatasetPreferencesListItem dataset={item}/>}
-      />
+      <View style={{flex: 1}}>
+        <SectionDivider
+          dividerText={`Datasets for project:  ${truncateText(project.description.project_name, 20)}`}/>
+        <FlatList
+          ItemSeparatorComponent={FlatListItemSeparator}
+          data={Object.values(datasets)}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <DatasetPreferencesListItem dataset={item}/>}
+        />
+      </View>
     </View>
   );
 };
