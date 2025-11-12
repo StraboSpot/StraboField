be255b771c15a5341ea875df6a919a73311c84a5
Nathan Novak <nnovak@ku.edu>
2025-11-12 12:58:22 -0700

    **refactor(app): Rename project and update references to 'StraboField'**
    - Renamed project from 'StraboSpot2' to 'StraboField' in `AppDelegate.swift`, `package.json`, `app.json`, and other relevant files.
    - Updated Podfile settings and dependencies for compatibility.
    - Adjusted app metadata, including displayName and moduleName, to reflect the new name.

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "StraboSpot2",
+      withModuleName: "StraboField",
       in: window,
       launchOptions: launchOptions
     )
